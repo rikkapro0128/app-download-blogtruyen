0d74f850d28ce514f9047b8b4ab1afeeb2b1eb38
rikapro0128 <stkssc002@gmail.com>
2022-06-10 08:03:34 +0700

    clean code, fix structe file, diretory, apply webpack build compress sass, css

---
+++ src/render/assets/fonts/index.js
@@ -0,0 +1,1 @@
+import './OverpassMono-Regular.ttf';
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,39 @@
 const path = require('path');
 
 module.exports = {
-  entry: '/src/public/index.js',
+  entry: {
+    render: '/src/render/index.js',
+    styles: '/src/render/style/index.js',
+    assets: '/src/render/assets/index.js',
+  },
   output: {
-    filename: 'bundler.min.js',
+    filename: '[name].min.js',
     path: path.resolve(__dirname, 'src/public'),
   },
   mode: 'development',
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: { outputStyle: 'compressed' },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|ttf|woff)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[folder]/[name].[ext]',
+        },
+      },
+    ],
+  },
 };
