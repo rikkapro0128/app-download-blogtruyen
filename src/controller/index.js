f3400248b407c5195727733810752811b18e19a5
rikapro0128 <stkssc002@gmail.com>
2022-06-15 11:50:40 +0700

    update function download manga, fix bug

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,8 @@ const { dialog, BrowserWindow, ipcMain } = require('electron');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const miruQuery = require('../helper/index.js');
+const miruQuery = require('../helper/queryDom.js');
+const miruHelp = require('../helper/hotFunc.js');
 
 class handleEvent {
   /*
@@ -20,127 +21,153 @@ class handleEvent {
     const cloneChapterStart = undefined || undefined; // undefined or typeof number
     const cloneChapterEnd = undefined || undefined; // undefined or typeof number
 
-    // init new chrome process
-    const browser = await puppeteer.launch({
-      headless: true,
-      // headless: false,
-      args: ['--disable-software-rasterizer', '--disable-gpu'],
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Init puppeteer process' });
+    const rootPath = 'E:/Download';
 
-    // wait for page load content done!
-    const page = await browser.newPage();
-    await page.goto(mangaTarget);
-    await page.waitForSelector('.manga-detail');
-    mainWindow.webContents.send('miru:update-task', { name: 'Puppeteer process loaded page' });
+    if (rootPath) {
+      // init new chrome process
+      const browser = await puppeteer.launch({
+        headless: true,
+        // headless: false,
+        args: ['--disable-software-rasterizer', '--disable-gpu'],
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Init puppeteer process' });
 
-    // query get content from DOM
+      // wait for page load content done!
+      const page = await browser.newPage();
+      await page.goto(mangaTarget);
+      await page.waitForSelector('.manga-detail');
+      mainWindow.webContents.send('miru:update-task', { name: 'Puppeteer process loaded page' });
 
-    const nameManga = await miruQuery.prop({ page, query: '.entry-title > a', property: 'textContent' });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get name manga', type: 'nameManga' });
+      // query get content from DOM
 
-    const thumbNailManga = await miruQuery.attr({ page, query: '.thumbnail > img', attribute: 'src' });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get thumbnail manga', type: 'thumbNailManga' });
+      const nameManga = await miruQuery.prop({ page, query: '.entry-title > a', property: 'textContent' });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get name manga', type: 'nameManga' });
 
-    const descManga = await miruQuery.prop({ page, query: '.detail > .content', property: 'textContent' });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get thumbnail manga', type: 'descManga' });
+      const thumbNailManga = await miruQuery.attr({ page, query: '.thumbnail > img', attribute: 'src' });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get thumbnail manga', type: 'thumbNailManga' });
 
-    const nameOther = await miruQuery.prop({
-      page,
-      query: '.description span.color-red',
-      property: 'textContent',
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get name other manga', type: 'nameOther' });
+      const descManga = await miruQuery.prop({ page, query: '.detail > .content', property: 'textContent' });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get thumbnail manga', type: 'descManga' });
 
-    const nameAuthor = await miruQuery.prop({
-      page,
-      query: '.description a.label.label-info',
-      property: 'textContent',
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get name author manga', type: 'nameAuthor' });
+      const nameOther = await miruQuery.prop({
+        page,
+        query: '.description span.color-red',
+        property: 'textContent',
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get name other manga', type: 'nameOther' });
 
-    const teamTranslate = await miruQuery.prop({
-      page,
-      query: '.description span.translater a',
-      property: 'textContent',
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get name team translate manga', type: 'teamTranslate' });
+      const nameAuthor = await miruQuery.prop({
+        page,
+        query: '.description a.label.label-info',
+        property: 'textContent',
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get name author manga', type: 'nameAuthor' });
 
-    // get category of manga
-    const categorys = await miruQuery.mix({
-      page,
-      method: '$$eval',
-      query: '.description p span.category a',
-      cb(element) {
-        return element.map((category) => category?.textContent);
-      },
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get categorys of manga', type: 'categorys' });
+      const teamTranslate = await miruQuery.prop({
+        page,
+        query: '.description span.translater a',
+        property: 'textContent',
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get name team translate manga', type: 'teamTranslate' });
 
-    // get user post manga of manga on blogtruyen
-    const userPost = await miruQuery.mix({
-      page,
-      query: '.description p a.color-u-1',
-      cb(element) {
-        return {
-          name: String(element.textContent).trim(),
-          linkUserOnBlogTruyen: element.getAttribute('href'),
-        };
-      },
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get user post this manga', type: 'userPost' });
-    // get user post last entry update manga of manga on blogtruyen
-    const lastEntryUpdate = await miruQuery.mix({
-      page,
-      query: '.description .col-sm-6 span.color-green',
-      cb(element) {
-        const dateTemp = String(element.textContent).trim().split(' ');
-        return {
-          data: dateTemp[0],
-          hour: dateTemp[1],
-        };
-      },
-    });
-    mainWindow.webContents.send('miru:update-task', {
-      name: 'Get last entry update this manga',
-      type: 'lastEntryUpdate',
-    });
-    // get all element chapter of manga on blogtruyen
-    const chapters = await miruQuery.mix({
-      page,
-      method: '$$eval',
-      query: '#list-chapters > p > span.title > a',
-      cb(elements, cloneChapterStart, cloneChapterEnd) {
-        elements = elements.reverse();
-        const list = elements.map((item, index) => {
-          if (
-            (cloneChapterStart === undefined && cloneChapterEnd === undefined) ||
-            (index + 1 >= cloneChapterStart && index + 1 <= cloneChapterEnd)
-          ) {
-            return {
-              nameChapter: item.textContent,
-              linkChapter: 'https://blogtruyen.vn' + item.getAttribute('href'),
-            };
-          }
-        });
-        return list.filter((value) => value !== null);
-      },
-      args: [cloneChapterStart, cloneChapterEnd],
-    });
-    mainWindow.webContents.send('miru:update-task', { name: 'Get all chapter manga', type: 'chapters' });
+      // get category of manga
+      const categorys = await miruQuery.mix({
+        page,
+        method: '$$eval',
+        query: '.description p span.category a',
+        cb(element) {
+          return element.map((category) => category?.textContent);
+        },
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get categorys of manga', type: 'categorys' });
 
-    // console.log({
-    //   nameManga: nameManga,
-    //   thumbNail: thumbNail,
-    //   elementDesc: elementDesc,
-    //   elementNameOther: elementNameOther,
-    //   elementNameAuthor: elementNameAuthor,
-    //   elementTeamTranslate: elementTeamTranslate,
-    //   elementCategory,
-    //   userPost,
-    //   lastEntryUpdate,
-    //   chapters,
-    // });
+      // get user post manga of manga on blogtruyen
+      const userPost = await miruQuery.mix({
+        page,
+        query: '.description p a.color-u-1',
+        cb(element) {
+          return {
+            name: String(element.textContent).trim(),
+            linkUserOnBlogTruyen: element.getAttribute('href'),
+          };
+        },
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get user post this manga', type: 'userPost' });
+      // get user post last entry update manga of manga on blogtruyen
+      const lastEntryUpdate = await miruQuery.mix({
+        page,
+        query: '.description .col-sm-6 span.color-green',
+        cb(element) {
+          const dateTemp = String(element.textContent).trim().split(' ');
+          return {
+            data: dateTemp[0],
+            hour: dateTemp[1],
+          };
+        },
+      });
+      mainWindow.webContents.send('miru:update-task', {
+        name: 'Get last entry update this manga',
+        type: 'lastEntryUpdate',
+      });
+      // get all element chapter of manga on blogtruyen
+      const chapters = await miruQuery.mix({
+        page,
+        method: '$$eval',
+        query: '#list-chapters > p > span.title > a',
+        cb(elements, cloneChapterStart, cloneChapterEnd) {
+          elements = elements.reverse();
+          const list = elements.map((item, index) => {
+            if (
+              (cloneChapterStart === undefined && cloneChapterEnd === undefined) ||
+              (index + 1 >= cloneChapterStart && index + 1 <= cloneChapterEnd)
+            ) {
+              return {
+                nameChapter: item.textContent,
+                linkChapter: 'https://blogtruyen.vn' + item.getAttribute('href'),
+              };
+            }
+          });
+          return list.filter((value) => value !== null);
+        },
+        args: [cloneChapterStart, cloneChapterEnd],
+      });
+      mainWindow.webContents.send('miru:update-task', { name: 'Get all chapter manga', type: 'chapters' });
+
+      const generalChapters = await miruHelp.handleChapters({ chapters, browser, window: mainWindow });
+
+      await browser.close();
+
+      mainWindow.webContents.send('miru:update-task', { name: 'Exit puppeteer browser', type: 'exitPuppeteerBrowser' });
+
+      let pathManga = `${rootPath}/${miruHelp.removeAccents(nameManga)}`;
+
+      if (fs.existsSync(pathManga)) {
+        // remove directory
+        fs.rmSync(pathManga, { recursive: true, force: true });
+      }
+      fs.mkdirSync(pathManga, { recursive: true });
+
+      mainWindow.webContents.send('miru:update-task', { name: 'Create path save manga', type: 'createPathSaveManga' });
+
+      const infoResponseFromDownload = await miruHelp.downloadChapters({
+        chapters: generalChapters,
+        pathManga,
+        window: mainWindow,
+      });
+
+      // console.log({
+      //   nameManga: nameManga,
+      //   thumbNail: thumbNailManga,
+      //   elementDesc: descManga,
+      //   elementNameOther: nameOther,
+      //   elementNameAuthor: nameAuthor,
+      //   elementTeamTranslate: teamTranslate,
+      //   categorys,
+      //   userPost,
+      //   lastEntryUpdate,
+      //   chapters,
+      // });
+    }
   }
 
   async choosePathSave({ event, arg, mainWindow }) {
---
+++ src/helper/hotFunc.js
@@ -0,0 +1,145 @@
+const miruQuery = require('../helper/queryDom.js');
+const fs = require('fs');
+const axios = require('axios');
+
+class miruHelp {
+  async handleChapters({ chapters, browser, window }) {
+    let status = false; // check and reload clone when a chapter exist error
+
+    // for (const chapter of chapters) {
+    for (const [index, chapter] of chapters.entries()) {
+      do {
+        try {
+          const page = await browser.newPage();
+          await page.goto(chapter.linkChapter);
+          await page.waitForSelector('#content');
+          const images = await miruQuery.mix({
+            page,
+            method: '$$eval',
+            query: '#content > img',
+            cb(elements) {
+              return elements.map((item) => item.getAttribute('src'));
+            },
+          });
+          chapters[index].listImages = images;
+          await page.close();
+          window.webContents.send('miru:update-task', {
+            name: `Append list image by chapter index: ${index}`,
+            type: 'appendListImage',
+          });
+          status = false;
+        } catch (error) {
+          // emit error to web-content
+          status = true;
+        }
+      } while (status);
+    }
+    return chapters;
+  }
+
+  async downloadFile({ url, pathByName }) {
+    return new Promise((res, rej) => {
+      axios({
+        method: 'get',
+        url: url,
+        headers: {
+          referer: 'https://blogtruyen.vn/',
+        },
+        responseEncoding: 'base64',
+        responseType: 'stream',
+      })
+        .then(function (response) {
+          let typefile = response.headers['content-type'];
+          let namePathImage = pathByName + `.${typefile === 'image/jpeg' ? 'jpg' : typefile.split('/')[1]}`;
+          response.data.pipe(
+            fs
+              .createWriteStream(namePathImage, {
+                autoClose: true,
+                encoding: 'base64',
+              })
+              .on('error', (err) => {
+                rej(err);
+              })
+              .on('finish', () => {
+                // check size download is full-filled
+                let stats = fs.statSync(namePathImage);
+                res({
+                  namePathImage: namePathImage,
+                  typeImage: response.headers['content-type'],
+                  sizeImage: response.headers['content-length'],
+                  fileSize: stats.size,
+                });
+              }),
+          );
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
+  }
+
+  async downloadChapters({ chapters, pathManga, window, maxStream }) {
+    const maxNumberStream = 2 || maxStream;
+    let infoAdressImage = new Array();
+    let chunkLink = new Array();
+    let stack = 0;
+
+    return new Promise(async (res, rej) => {
+      try {
+        for (let indexChapters = 0; indexChapters < chapters.length; indexChapters++) {
+          // loop chapters
+
+          let listImageChapter = chapters[indexChapters].listImages;
+          let lenImageChapter = listImageChapter.length;
+
+          let initPathChapter = `${pathManga}/${this.removeAccents(
+            chapters[indexChapters].nameChapter.split(' ').join('-'),
+          )}/`;
+          fs.mkdirSync(initPathChapter, { recursive: true });
+
+          // console.log(initPathChapter);
+
+          for (let indexImage = 0; indexImage < lenImageChapter; indexImage++) {
+            // loop image every chapter
+            stack++;
+            // console.log('Download with stack => ' + stack + ' => ' + listImageChapter[indexImage]);
+            chunkLink.push(listImageChapter[indexImage]); // push list images every chapter
+            // just download by max stack or end of last index
+            if (chunkLink.length === maxNumberStream || indexImage === lenImageChapter - 1) {
+              const checkList = chunkLink.map((link) => {
+                let nameFileByDate = initPathChapter + Date.now();
+                return this.downloadFile({ url: link, pathByName: nameFileByDate });
+              });
+
+              const infoImages = await Promise.allSettled(checkList);
+              infoAdressImage.push(...infoImages);
+              // console.log('<=< Finish stack! >=>');
+              chunkLink = new Array();
+              stack = 0;
+            }
+          }
+          chapters[indexChapters].infoListImage = infoAdressImage;
+          infoAdressImage = new Array();
+          window.webContents.send('miru:update-task', {
+            name: `Download done list image ${chapters[indexChapters].nameChapter}`,
+            type: 'increasePercent',
+          });
+        }
+        res(chapters);
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  removeAccents(str) {
+    return String(str)
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/đ/g, 'd')
+      .replace(/Đ/g, 'D')
+      .replace(/[`?!*~,<>;':"\/\[\]\|{}()=_+]/g, '');
+  }
+}
+
+module.exports = new miruHelp();
